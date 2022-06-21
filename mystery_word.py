6728ed77a4e9ed57c08b472c89ddc22446b3efe5
Lisa Williams <lisaismyname0@gmail.com>
2022-06-21 11:37:19 -0400

    beginnings of working with test-word.txt

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,16 @@
 def play_game():
-    pass
+    print("Let's Begin")
+    with open("test-word.txt") as test_word:
+        guess = test_word.read()
+        print(guess)
+        for letter in guess:
+            print(letter)
+
+        def show_guess_length():
+            for letter in guess:
+                i = len(guess)
+            print("_"*i)
+        show_guess_length()
 
 
 if __name__ == "__main__":
