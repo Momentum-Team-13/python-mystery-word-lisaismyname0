2faa40004367f2973884d426b8d82f51878f67ad
Lisa Williams <lisaismyname0@gmail.com>
2022-06-21 17:15:25 -0400

    need to fix that using word_dash as parameter in reveal_letters changes the value of word_dash

--- mystery_word.py
+++ mystery_word.py
@@ -1,49 +1,44 @@
+with open("test-word.txt") as test_word:
+    generated_word = test_word.read()
+    list_word = []
+    list_word[:0] = generated_word
+
+
+def start_game():
+    word_length = len(list_word)
+    underscore = "_"
+    word_dash = []
+    word_dash[:0] = underscore * word_length
+    print(f"Generated Word is: {underscore*word_length}")
+
 
 def play_game():
+    start_game()
     already_guessed = []
-    print("Let's Begin")
-    with open("test-word.txt") as test_word:
-        generated_word = test_word.read()
-    word_length = len(generated_word)
-    print(generated_word)
-    underscore = "_"
-    word_dash = underscore*word_length
-    print(f"Generated Word is: {word_dash}")
-
     current_guess = input("Guess a letter:")
+    if current_guess in list_word:
+        reveal_letters(current_guess, list_word)
+    else:
+        print("Nope! Keep Goin")
+
     if current_guess in already_guessed:
-        print("Don't Double Guess!")
+        print("No Double Guesses!")
     else:
         already_guessed.append(current_guess)
-    print(current_guess)
-    if current_guess in generated_word:
-        print(generated_word)
-        reveal_letters(current_guess, generated_word)
-    else:
-        print("keep goin")
-
     print(already_guessed)
 
 
-play_game()
-
-
-def reveal_letters(current_guess, generated_word):
-    for letter in generated_word:
+def reveal_letters(current_guess, word_dash):
+    print(word_dash)
+    for letter in list_word:
         if current_guess == letter:
-            specific_index = generated_word.index(letter)
-            print(specific_index)
             print(f"you correctly guessed letter: {letter}")
-            # for dash in word_dash:
-            """split = word_dash.split()
-                print(split[specific_index])
-                needs_replacing = split[specific_index].replace(
-                    "_", f"{letter}")
-                generated_word = needs_replacing.join("")"""
-            # print(type(dash))
-            # print(word_dash)
-            print(generated_word)
+            specific_index = word_dash.index(letter)
+            print(specific_index)
+            word_dash[specific_index].replace("_", letter)
+    print(f"this is {word_dash}")
 
 
+# add while loop to establish conditions of play_game() repeating (user runs out of guesses or user guesses word)
 if __name__ == "__main__":
     play_game()
